a29e1a929955b49422f98b4555bb7e7a78b6f635
Alumno <Alumno@10.0.2.15>
2021-04-23 17:21:32 +0200

    Metodo 1

--- actividad4/src/AleatorioEmple.java
+++ actividad4/src/AleatorioEmple.java
@@ -7,6 +7,7 @@ public class AleatorioEmple {
 	public static void main(String[] args) throws IOException {
 		crear();
 		leer();
+		System.out.print("");
 	}
 
 	public static void leer() throws IOException {
